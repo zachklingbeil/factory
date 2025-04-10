cca6acd825adcdc46170537679f8ca9311a7b62f
zachklingbeil <zach@zachklingbeil.com>
2025-04-10 12:58:28 -0700

    forgot to call consolidate

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func NewFactory(dbName string) (*Factory, error) {
 	}
 	db, _ := fx.NewDatabase(dbName)
 	fmt.Printf("factory [ %s ]\n", dbName)
+	db.Consolidate()
 
 	peer := peer.NewPeers(json, eth, db)
 	// go peer.HelloUniverse()

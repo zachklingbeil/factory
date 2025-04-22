58934955d761d5d817afafe92636a1aea95ca045
zachklingbeil <zach@zachklingbeil.com>
2025-04-22 12:12:30 -0700

    coordinates, index method moved to circuit

--- fx/circuit.go
+++ fx/circuit.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"maps"
 	"sync"
+	"time"
 )
 
 type Circuit struct {
-	One map[Zero]any
-	Ctx context.Context
-	Mu  *sync.Mutex
+	One  map[any]any
+	Zero map[Zero]any
+	Ctx  context.Context
+	Mu   *sync.Mutex
 }
 
 type Zero struct {
@@ -26,7 +28,7 @@ type Zero struct {
 
 func NewCircuit(ctx context.Context, mu *sync.Mutex) *Circuit {
 	circuit := &Circuit{
-		One: make(map[Zero]any),
+		One: make(map[any]any),
 		Ctx: ctx,
 		Mu:  mu,
 	}
@@ -34,10 +36,9 @@ func NewCircuit(ctx context.Context, mu *sync.Mutex) *Circuit {
 }
 
 // Add safely adds one to Circuit.One
-func (c *Circuit) Add(one map[Zero]any) {
+func (c *Circuit) Add(one map[any]any) {
 	c.Mu.Lock()
 	defer c.Mu.Unlock()
-
 	maps.Copy(c.One, one)
 }
 
@@ -46,4 +47,26 @@ func (c *Circuit) Read(zero Zero) any {
 	defer c.Mu.Unlock()
 	value := c.One[zero]
 	return value
+}
+
+func (c *Circuit) Coordinates(blockNumber, timestamp int64, ones []any) (Zero, any, error) {
+	for i := range ones {
+		if tx, ok := ones[i].(map[string]any); ok {
+			tx["index"] = i + 1
+		}
+	}
+
+	t := time.UnixMilli(timestamp)
+	coord := Zero{
+		Block:       blockNumber,
+		Year:        uint8(t.Year() - 2015),
+		Month:       uint8(t.Month()),
+		Day:         uint8(t.Day()),
+		Hour:        uint8(t.Hour()),
+		Minute:      uint8(t.Minute()),
+		Second:      uint8(t.Second()),
+		Millisecond: uint16(t.Nanosecond() / 1e6),
+		Index:       0,
+	}
+	return coord, ones, nil
 }

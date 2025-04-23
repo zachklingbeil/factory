913c6e6313f32e48dc7c4deed6c8cb0516e0c185
zachklingbeil <zach@zachklingbeil.com>
2025-04-23 10:48:07 -0700

    commit

--- fx/db.go
+++ fx/db.go
@@ -19,56 +19,55 @@ type Database struct {
 	Ctx context.Context
 }
 
-func Connect(dbName string, dbNum int, ctx context.Context, mu *sync.Mutex, rw *sync.RWMutex) (*Database, error) {
-	db := &Database{
-		Mu:  mu,
-		Rw:  rw,
-		Ctx: ctx,
-	}
-
-	// Connect to Postgres
-	if err := db.ConnectPostgres(dbName); err != nil {
-		return nil, err
-	}
-
-	// Connect to Redis
-	client, err := db.ConnectRedis(dbNum)
-	if err != nil {
-		return nil, err
-	}
-	db.Rdb = client // Assign the Redis client to the Database struct
-
-	return db, nil
-}
-
-func (db *Database) ConnectPostgres(dbName string) error {
+func ConnectPostgres(dbName string) (*sql.DB, error) {
 	connStr := fmt.Sprintf("user=postgres password=postgres dbname=%s host=postgres port=5432 sslmode=disable", dbName)
 	pg, err := sql.Open("postgres", connStr)
 	if err != nil {
-		return fmt.Errorf("failed to open connection to database '%s': %w", dbName, err)
+		return nil, fmt.Errorf("failed to open connection to database '%s': %w", dbName, err)
 	}
 
 	const maxRetries = 5
 	for i := 1; i <= maxRetries; i++ {
 		if err := pg.Ping(); err == nil {
-			db.Pg = pg
-			return nil
+			return pg, nil
 		}
 		fmt.Printf("Retrying connection to database '%s' (%d/%d)...\n", dbName, i, maxRetries)
 		time.Sleep(time.Second * time.Duration(i*2))
 	}
-	return fmt.Errorf("failed to connect to database '%s' after %d retries", dbName, maxRetries)
+	return nil, fmt.Errorf("failed to connect to database '%s' after %d retries", dbName, maxRetries)
 }
 
-func (db *Database) ConnectRedis(dbNumber int) (*redis.Client, error) {
+func ConnectRedis(dbNumber int, ctx context.Context) (*redis.Client, error) {
 	pw := os.Getenv("PASSWORD")
 	client := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: pw,
 		DB:       dbNumber,
 	})
-	if _, err := client.Ping(db.Ctx).Result(); err != nil {
+	if _, err := client.Ping(ctx).Result(); err != nil {
 		return nil, fmt.Errorf("failed to connect to Redis: %w", err)
 	}
 	return client, nil
 }
+
+// func Connect(dbName string, dbNum int, ctx context.Context, mu *sync.Mutex, rw *sync.RWMutex) (*Database, error) {
+// 	db := &Database{
+// 		Mu:  mu,
+// 		Rw:  rw,
+// 		Ctx: ctx,
+// 	}
+
+// 	// Connect to Postgres
+// 	if err := db.ConnectPostgres(dbName); err != nil {
+// 		return nil, err
+// 	}
+
+// 	// Connect to Redis
+// 	client, err := db.ConnectRedis(dbNum)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	db.Rdb = client // Assign the Redis client to the Database struct
+
+// 	return db, nil
+// }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package factory
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"sync"
@@ -9,23 +10,26 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
 	"github.com/zachklingbeil/factory/fx"
 )
 
 type Factory struct {
-	Ctx     context.Context
-	Db      *fx.Database
-	Json    *fx.JSON
-	Circuit *fx.Circuit
-	Eth     *ethclient.Client
-	Http    *http.Client
-	Rpc     *rpc.Client
-	Mu      *sync.Mutex
-	Rw      *sync.RWMutex
-	When    *sync.Cond
+	Ctx   context.Context
+	Eth   *ethclient.Client
+	Http  *http.Client
+	Rpc   *rpc.Client
+	Redis *redis.Client
+	Pg    *sql.DB
+	Json  *fx.JSON
+	// Circuit *fx.Circuit
+	Math *fx.Math
+	Mu   *sync.Mutex
+	Rw   *sync.RWMutex
+	When *sync.Cond
 }
 
-func Assemble(dbName string, rdb int) *Factory {
+func Assemble(dbName string, dbNum int) *Factory {
 	ctx := context.Background()
 	http := &http.Client{}
 	json := fx.Json(*http, ctx)
@@ -38,23 +42,29 @@ func Assemble(dbName string, rdb int) *Factory {
 	rw := &sync.RWMutex{}
 	when := sync.NewCond(mu)
 
-	db, err := fx.Connect(dbName, rdb, ctx, mu, rw)
+	pg, err := fx.ConnectPostgres(dbName)
 	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
+		log.Fatalf("Error connecting to Postgres: %v", err)
 	}
-	circuit := fx.NewCircuit(ctx, mu)
 
+	redis, err := fx.ConnectRedis(dbNum, ctx)
+	if err != nil {
+		log.Fatalf("Error connecting to Redis: %v", err)
+	}
+	// circuit := fx.NewCircuit(ctx, mu)
 	factory := &Factory{
-		Ctx:     ctx,
-		Db:      db,
-		Json:    json,
-		Circuit: circuit,
-		Eth:     eth,
-		Http:    http,
-		Rpc:     rpc,
-		Mu:      mu,
-		Rw:      rw,
-		When:    when,
+		Ctx:   ctx,
+		Pg:    pg,
+		Redis: redis,
+		Json:  json,
+		// Circuit: circuit,
+		Eth:  eth,
+		Http: http,
+		Math: &fx.Math{},
+		Rpc:  rpc,
+		Mu:   mu,
+		Rw:   rw,
+		When: when,
 	}
 	return factory
 }

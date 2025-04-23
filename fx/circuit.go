6a56748e4c0d2591e32bd8652640bcda22edde82
zachklingbeil <zach@zachklingbeil.com>
2025-04-23 12:00:36 -0700

    commit

--- fx/circuit.go
+++ fx/circuit.go
@@ -2,29 +2,13 @@ package fx
 
 import (
 	"context"
-	"maps"
 	"sync"
-	"time"
 )
 
 type Circuit struct {
-	One  map[any]any
-	Zero map[Zero]any
-	Ctx  context.Context
-	Mu   *sync.Mutex
-}
-
-type Zero struct {
-	Block       int64  `json:"block"`
-	Year        uint8  `json:"year"`
-	Month       uint8  `json:"month"`
-	Day         uint8  `json:"day"`
-	Hour        uint8  `json:"hour"`
-	Minute      uint8  `json:"minute"`
-	Second      uint8  `json:"second"`
-	Millisecond uint16 `json:"millisecond"`
-	Index       uint16 `json:"index"`
-	Count       uint16 `json:"count"`
+	One map[any]any
+	Ctx context.Context
+	Mu  *sync.Mutex
 }
 
 func NewCircuit(ctx context.Context, mu *sync.Mutex) *Circuit {
@@ -36,39 +20,39 @@ func NewCircuit(ctx context.Context, mu *sync.Mutex) *Circuit {
 	return circuit
 }
 
-// Add safely adds one to Circuit.One
-func (c *Circuit) Add(one map[any]any) {
-	c.Mu.Lock()
-	defer c.Mu.Unlock()
-	maps.Copy(c.One, one)
-}
+// // Add safely adds one to Circuit.One
+// func (c *Circuit) Add(one map[any]any) {
+// 	c.Mu.Lock()
+// 	defer c.Mu.Unlock()
+// 	maps.Copy(c.One, one)
+// }
 
-func (c *Circuit) Read(zero Zero) any {
-	c.Mu.Lock()
-	defer c.Mu.Unlock()
-	value := c.One[zero]
-	return value
-}
+// func (c *Circuit) Read(zero Zero) any {
+// 	c.Mu.Lock()
+// 	defer c.Mu.Unlock()
+// 	value := c.One[zero]
+// 	return value
+// }
 
-func (c *Circuit) Coordinates(blockNumber, timestamp int64, ones []any) (Zero, []any, error) {
-	for i := range ones {
-		if tx, ok := ones[i].(map[string]any); ok {
-			tx["index"] = i + 1
-		}
-	}
-	count := len(ones)
-	t := time.UnixMilli(timestamp)
-	coord := Zero{
-		Block:       blockNumber,
-		Year:        uint8(t.Year() - 2015),
-		Month:       uint8(t.Month()),
-		Day:         uint8(t.Day()),
-		Hour:        uint8(t.Hour()),
-		Minute:      uint8(t.Minute()),
-		Second:      uint8(t.Second()),
-		Millisecond: uint16(t.Nanosecond() / 1e6),
-		Index:       0,
-		Count:       uint16(count),
-	}
-	return coord, ones, nil
-}
+// func (c *Circuit) Coordinates(blockNumber, timestamp int64, ones []any) (Zero, []any, error) {
+// 	for i := range ones {
+// 		if tx, ok := ones[i].(map[string]any); ok {
+// 			tx["index"] = i + 1
+// 		}
+// 	}
+// 	count := len(ones)
+// 	t := time.UnixMilli(timestamp)
+// 	coord := Zero{
+// 		Block:       blockNumber,
+// 		Year:        uint8(t.Year() - 2015),
+// 		Month:       uint8(t.Month()),
+// 		Day:         uint8(t.Day()),
+// 		Hour:        uint8(t.Hour()),
+// 		Minute:      uint8(t.Minute()),
+// 		Second:      uint8(t.Second()),
+// 		Millisecond: uint16(t.Nanosecond() / 1e6),
+// 		Index:       0,
+// 		Count:       uint16(count),
+// 	}
+// 	return coord, ones, nil
+// }

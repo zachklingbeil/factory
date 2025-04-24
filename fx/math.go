3234b0d9389194c07a0d40a83d577e7594ba69de
zachklingbeil <zach@zachklingbeil.com>
2025-04-24 14:25:01 -0700

    commit

--- fx/math.go
+++ fx/math.go
@@ -1,6 +1,7 @@
 package fx
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/log"
@@ -14,4 +15,18 @@ func (m *Math) Int(value string) *big.Int {
 		log.Error("Failed to convert string to big.Int: %s", value)
 	}
 	return bigIntValue
+}
+
+func (m *Math) Up(high, max int64) error {
+	for i := high; i <= max; i++ {
+		fmt.Println(i)
+	}
+	return nil
+}
+
+func (m *Math) Down(low int64) error {
+	for i := low; i >= 1; i-- {
+		fmt.Println(i)
+	}
+	return nil
 }

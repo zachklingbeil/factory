2c2fec38f5b23e6d946bd952fde0a1f9f8d27f6e
zachklingbeil <zach@zachklingbeil.com>
2025-07-08 09:00:35 -0700

    commit

--- universe/universe.go
+++ universe/universe.go
@@ -1,40 +1,30 @@
 package universe
 
-import (
-	"embed"
-	"net/http"
-
-	"github.com/zachklingbeil/factory/universe/element"
-)
-
-//go:embed index.html
-var content embed.FS
-
 type Universe struct {
-	Element *element.Element
+	Element *Element
 	Index   string
 }
 
 func New() *Universe {
 	return &Universe{
-		Element: element.NewElements(),
+		Element: NewElements(),
 	}
 }
 
-// ServeIndex serves the embedded index.html file.
-func (u *Universe) ServeIndex(w http.ResponseWriter, r *http.Request) {
-	data, err := content.ReadFile("index.html")
-	if err != nil {
-		http.Error(w, "index.html not found", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write(data)
-}
+// // ServeIndex serves the embedded index.html file.
+// func (u *Universe) ServeIndex(w http.ResponseWriter, r *http.Request) {
+// 	data, err := content.ReadFile("index.html")
+// 	if err != nil {
+// 		http.Error(w, "index.html not found", http.StatusInternalServerError)
+// 		return
+// 	}
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	w.Write(data)
+// }
 
-// ServeMainFragment serves HTML content for the <main> element.
-func (u *Universe) ServeMainFragment(w http.ResponseWriter, r *http.Request) {
-	fragment := `<h2>Welcome to the Home Page!</h2><p>This is server-rendered content.</p>`
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write([]byte(fragment))
-}
+// // ServeMainFragment serves HTML content for the <main> element.
+// func (u *Universe) ServeMainFragment(w http.ResponseWriter, r *http.Request) {
+// 	fragment := `<h2>Welcome to the Home Page!</h2><p>This is server-rendered content.</p>`
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	w.Write([]byte(fragment))
+// }

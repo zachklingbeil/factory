2e2a33afca33e596734c0a01234900c266288abf
zachklingbeil <zach@zachklingbeil.com>
2025-07-16 15:31:21 -0700

    commit

--- frame/frame.go
+++ frame/frame.go
@@ -21,32 +21,24 @@ func (f *Frame) CreateFrame(elements ...template.HTML) template.HTML {
 	if len(elements) == 0 {
 		return template.HTML("")
 	}
-
 	var builder strings.Builder
 	for _, element := range elements {
 		builder.WriteString(string(element))
 	}
-
 	return template.HTML(builder.String())
 }
 
-func (f *Frame) AddCSS(styles map[string]string) template.HTML {
+func (f *Frame) AddCSS(styles map[string]string) template.CSS {
 	var builder strings.Builder
-	builder.WriteString("<style>")
 	for selector, rules := range styles {
 		builder.WriteString(selector)
 		builder.WriteString(" { ")
 		builder.WriteString(rules)
 		builder.WriteString(" }\n")
 	}
-	builder.WriteString("</style>")
-	return template.HTML(builder.String())
+	return template.CSS(builder.String())
 }
 
-func (f *Frame) AddJS(js string) template.HTML {
-	var builder strings.Builder
-	builder.WriteString("<script>")
-	builder.WriteString(js)
-	builder.WriteString("</script>")
-	return template.HTML(builder.String())
+func (f *Frame) AddJS(js string) template.JS {
+	return template.JS(js)
 }

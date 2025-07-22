99fa2173aee3a0d2060bf4db0dad532a800a1fb7
zachklingbeil <zach@zachklingbeil.com>
2025-07-22 12:36:33 -0700

    commit

--- one/one.go
+++ one/one.go
@@ -23,16 +23,32 @@ func NewOne() *One {
 
 	return one
 }
-
-func (o *One) Pathless(cssPath, domain string, frames int) {
-	pathless := o.BuildPathless(cssPath, domain, frames)
-	o.Frames = append(o.Frames, &pathless)
+func (o *One) ServeRoot() {
 	o.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte(pathless))
+		w.Write([]byte(*o.Frames[0]))
 	})
 }
 
+func (o *One) Pathless(cssPath, domain string, frames int) {
+	frame := o.BuildPathless(cssPath, domain, frames)
+	o.Frames = append(o.Frames, &frame)
+}
+
+// Serve frames at /frame/{index}
+func (o *One) ServeFrames() {
+	o.HandleFunc("/frame/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Header().Set("X-FRAMES", strconv.Itoa(len(o.Frames)))
+		idxStr := strings.TrimPrefix(r.URL.Path, "/frame/")
+		idx, err := strconv.Atoi(idxStr)
+		if err != nil || idx < 0 || idx >= len(o.Frames) {
+			http.Error(w, "Frame not found", http.StatusNotFound)
+			return
+		}
+		w.Write([]byte(*o.Frames[idx]))
+	})
+}
 func (o *One) AddPath(dir, prefix string) error {
 	files, err := o.SourcePath(dir)
 	if err != nil {
@@ -48,18 +64,4 @@ func (o *One) AddPath(dir, prefix string) error {
 		})
 	}
 	return nil
-}
-
-func (o *One) Index() {
-	o.HandleFunc("/frame/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Header().Set("X-FRAMES", strconv.Itoa(len(o.Frames)))
-		idxStr := strings.TrimPrefix(r.URL.Path, "/frame/")
-		idx, err := strconv.Atoi(idxStr)
-		if err != nil || idx < 0 || idx >= len(o.Frames) {
-			http.Error(w, "Frame not found", http.StatusNotFound)
-			return
-		}
-		w.Write([]byte(*o.Frames[idx]))
-	})
 }

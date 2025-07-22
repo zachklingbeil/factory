0f5967f8a8382f95bb7d91e34640f181866dfaa9
zachklingbeil <zach@zachklingbeil.com>
2025-07-22 12:24:50 -0700

    commit

--- one/one.go
+++ one/one.go
@@ -46,26 +46,13 @@ func (o *One) AddPath(dir, prefix string) error {
 	return nil
 }
 
-// frame[0] is served at the root path ("/"), frame[1] and onwards are served at "/frame/{index}"
 func (o *One) Index() {
-	// Serve index 0 at "/"
-	o.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Header().Set("X-FRAMES", strconv.Itoa(len(o.Frames)))
-		if len(o.Frames) == 0 {
-			http.Error(w, "No frame available", http.StatusNotFound)
-			return
-		}
-		w.Write([]byte(*o.Frames[0]))
-	})
-
-	// Serve other frames at /frame/{index}
 	o.HandleFunc("/frame/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		w.Header().Set("X-FRAMES", strconv.Itoa(len(o.Frames)))
 		idxStr := strings.TrimPrefix(r.URL.Path, "/frame/")
 		idx, err := strconv.Atoi(idxStr)
-		if err != nil || idx < 1 || idx >= len(o.Frames) {
+		if err != nil || idx < 0 || idx >= len(o.Frames) {
 			http.Error(w, "Frame not found", http.StatusNotFound)
 			return
 		}
